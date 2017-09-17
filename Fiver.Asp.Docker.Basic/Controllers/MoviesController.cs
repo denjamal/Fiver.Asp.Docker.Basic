a064c13165d1b7d2082551c3dd2dd71ce326469d
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-17 19:16:32 +0100

    initial

---
+++ Fiver.Asp.Docker.Basic/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Asp.Docker.Basic.Controllers
+{
+    [Route("api/movies")]
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from inside Docker!");
+        }
+    }
+}
