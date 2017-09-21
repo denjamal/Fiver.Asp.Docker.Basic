ca83080ac67599a890ff5af31b5407d1c0d04d37
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-21 09:37:58 +0100

    added test project

---
+++ Fiver.Asp.Docker.Basic.Tests/MoviesControllerTests.cs
@@ -0,0 +1,26 @@
+using Fiver.Asp.Docker.Basic.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Fiver.Asp.Docker.Basic.Tests
+{
+    public class MoviesControllerTests
+    {
+        [Fact(DisplayName = "Get_retruns_OkObjectResult_and_model")]
+        public void Get_retruns_Ok_result_and_model()
+        {
+            // Arrange
+            var sut = new MoviesController();
+
+            // Act
+            var result = sut.Get();
+
+            // Assert
+            var okObjectResult = Assert.IsType<OkObjectResult>(result);
+            var outputModel = Assert.IsType<string>(okObjectResult.Value);
+            Assert.Equal(
+                expected: "Hello from inside Docker!",
+                actual: outputModel);
+        }
+    }
+}
